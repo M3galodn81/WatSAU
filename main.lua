408b1108f7ebef0634851358835e98b4ddb8e1ac
M3galodn81 <dalisdism0@gmail.com>
2025-03-13 14:01:59 +0800

    inital commit

---
+++ main.lua
@@ -0,0 +1,147 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Surtr Mod
+--- MOD_ID: SurtrMod
+--- MOD_AUTHOR: [M3galodn81]
+--- MOD_DESCRIPTION: Peak Pain
+--- PREFIX: surtr
+----------------------------------------------
+------------ MOD CODE -------------------------
+
+SMODS.Atlas{
+    key = 'jokers', --atlas key
+    path = 'jokers.png', --atlas' path in (yourMod)/assets/1x or (yourMod)/assets/2x
+    px = 71, --width of one card
+    py = 95 -- height of one card
+}
+
+SMODS.Joker{
+    key = 'test_joker',
+    loc_txt = {
+        name = 'Test Joker',
+        text = {
+            'When Blind is selected,',
+            'creates a {C:attention}Perkeo{}',
+            '{X:mult,C:white}X#1#{} Mult',
+            'Gain {C:money}123${} at end of round'
+        }
+    },
+    atlas = 'jokers', --atlas' key
+    pos = {x = 0, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    rarity = 4, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 100, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
+    blueprint_compat = true, --can it be blueprinted/brainstormed/other
+    eternal_compat = false, --can it be eternal
+    perishable_compat = false, --can it be perishable
+    
+    config = { 
+        extra = {
+            Xmult = 100 --configurable value
+        }
+    },
+    loc_vars = function(self,info_queue,center)
+        info_queue[#info_queue+1] = G.P_CENTERS.j_perkeo --adds "Joker"'s description next to this card's description
+        return {vars = {center.ability.extra.Xmult}} --#1# is replaced with card.ability.extra.Xmult
+    end,
+
+    calculate = function(self,card,context)
+        if context.joker_main then
+            return {
+                card = card,
+                Xmult_mod = card.ability.extra.Xmult,
+                message = 'X' .. card.ability.extra.Xmult,
+                colour = G.C.MULT
+            }
+        end
+
+        if context.setting_blind and #G.jokers.cards + G.GAME.joker_buffer < G.jokers.config.card_limit then
+            local new_card = create_card('Perkeo', G.jokers, nil,nil,nil,nil,'j_perkeo')
+            new_card:add_to_deck()
+            G.jokers:emplace(new_card)
+        end
+    end,
+
+    calc_dollar_bonus = function(self,card) --for gain money at end of the round
+        return 123
+    end,
+}
+
+
+SMODS.Joker{
+    key = 'haze',
+    loc_txt = {
+        name = 'Haze',
+        text = {
+            'If a {C:attention}poker hand played{} is a {C:attention}High Card{},',
+            'creates a random {C:blue}Spectral{} Card',
+            'and gives {X:mult,C:white}X#1#{} Mult',
+            'otherwise it will {C:attention}destroy{} the {C:attention}played hand{}',
+        }
+    },
+    atlas = 'jokers', --atlas' key
+    pos = {x = 1, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    rarity = 4, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 6, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
+    blueprint_compat = false, --can it be blueprinted/brainstormed/other
+    eternal_compat = false, --can it be eternal
+    perishable_compat = false, --can it be perishable
+    
+    config = { 
+        extra = {
+            Xmult = 2,--configurable v
+        }
+    },
+    loc_vars = function(self,info_queue,center)
+        return {vars = {center.ability.extra.Xmult}} --#1# is replaced with card.ability.extra.Xmult
+    end,
+
+    calculate = function(self,card,context)
+        if context.joker_main then
+            return {
+                card = card,
+                Xmult_mod = card.ability.extra.Xmult,
+                message = 'X' .. card.ability.extra.Xmult .. ' Mult',
+                colour = G.C.MULT
+            }
+        end
+
+        
+        if context.before and next(context.poker_hands['High Card']) and not context.blueprint then
+
+            if context.scoring_name == "High Card" then
+
+                if #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                    card = create_card('Spectral',G.consumeables, nil, nil, nil, nil, nil, '8ba')
+                    card:add_to_deck()
+                    G.consumeables:emplace(card)
+                else
+                    return {
+                        message = 'No Room!',
+                        colour = G.C.YELLOW     
+                    }
+                    
+                end 
+            else 
+                    return {
+                        message = 'Invalid!', colour = G.C.YELLOW
+                    }
+            end
+        end
+    end,
+
+    -- calc_dollar_bonus = function(self,card) --for gain money at end of the round
+    --     return 123
+    -- end,
+}
+
+
+
+
+
+------------ MOD CODE END ----------------------
+----------------------------------------------
