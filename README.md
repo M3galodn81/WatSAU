97d78d7e0c4ad39558e43e320653800f03c639cf
M3galodn81 <dalisdism0@gmail.com>
2025-03-13 23:01:22 +0800

    Added Der RIchter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Surtr Mod
 
-WIP Balatro Mod
+A Balatro Mod where I added random or broken jokers 
 
 WIP description
 
@@ -8,4 +8,7 @@ WIP description
 
 # Installation Guide
 
-# ???
+# License
+
+### All assets are license under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+### The code is license under GNU General Public License v3.0 (GPL-3.0)
--- main.lua
+++ main.lua
@@ -153,6 +153,100 @@ SMODS.Joker{
     -- end,
 }
 
+--Der Richter
+SMODS.Joker{
+    key = 'der_richter',
+    loc_txt = {
+        name = 'Der Richter',
+        text = {
+            'Balance {C:chips}Chips{} and {C:mult}Mult{}',
+            'when this joker is triggered'
+        }
+    },
+    atlas = 'jokers', --atlas' key
+    pos = {x = 2, y = 0}, --position in atlas, starts at 0, scales by the atlas' card size (px and py): {x = 1, y = 0} would mean the sprite is 71 pixels to the right
+    rarity = 4, --rarity: 1 = Common, 2 = Uncommon, 3 = Rare, 4 = Legendary
+    --soul_pos = { x = 0, y = 0 },
+    cost = 100, --cost
+    unlocked = true, --where it is unlocked or not: if true, 
+    discovered = true, --whether or not it starts discovered
+    blueprint_compat = true, --can it be blueprinted/brainstormed/other
+    eternal_compat = false, --can it be eternal
+    perishable_compat = false, --can it be perishable
+    
+    config = { 
+    },
+    loc_vars = function(self,info_queue,center)
+    end,
+
+    calculate = function(self,card,context)
+
+        if context.joker_main then
+            -- total = hand_chips + mult
+            -- chip_balanced =  math.floor(total/2)
+            -- mult_balanced = math.floor(total/2)
+
+            -- print("After Cal of total")
+            -- print(hand_chips .. "chips")
+            -- print(mult       .. "mult")
+            -- print(total      .. "total")
+            -- print(chip_balanced .. "x" ..mult_balanced)
+
+            -- update_hand_text({delay = 0}, {mult = mult_balanced, chips = chips_balanced})
+            -- -- Plasma Animation
+            -- G.E_MANAGER:add_event(Event({
+            --     func = (function()
+                
+            --         play_sound('gong', 0.94, 0.3)
+            --         play_sound('gong', 0.94*1.5, 0.2)
+            --         play_sound('tarot1', 1.5)
+            --         ease_colour(G.C.UI_CHIPS, {0.8, 0.45, 0.85, 1})
+            --         ease_colour(G.C.UI_MULT, {0.8, 0.45, 0.85, 1})
+                
+            --         G.E_MANAGER:add_event(Event({
+            --             trigger = 'after',
+            --             blockable = false,
+            --             blocking = false,
+            --             delay =  4.3,
+            --             func = (function() 
+            --                     ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
+            --                     ease_colour(G.C.UI_MULT, G.C.RED, 2)
+            --                 return true
+            --             end)
+            --         }))
+            --         G.E_MANAGER:add_event(Event({
+            --             trigger = 'after',
+            --             blockable = false,
+            --             blocking = false,
+            --             no_delete = true,
+            --             delay =  6.3,
+            --             func = (function() 
+            --                 G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] = G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
+            --                 G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] = G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
+            --                 return true
+            --             end)
+            --         }))
+            --         return true
+            --     end)
+            -- }))
+
+
+            return {
+                card = card,
+                message = 'Balanced',
+                colour = G.C.PURPLE,
+                -- chips =  chip_balanced,
+                -- mult = mult_balanced
+            }
+            
+        end
+    end
+
+
+
+}
+
+
 
 
 
